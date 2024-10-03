e8411b4193c40dd3f8ea326b7bc7c3ec3c876022
ajizants <13kalong13@gmail.com>
2024-10-03 07:35:56 +0700

    fix layout login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -27,52 +27,62 @@
             margin: 0;
             padding: 0;
         }
+
+        .container2 {
+            position: absolute;
+            top: 50%;
+            left: 30%;
+            transform: translate(-50%, -50%);
+            scale: 1.2;
+        }
     </style>
 
 </head>
 
 <body>
-    <div class="container">
-        <div class="card border-primary shadow-lg my-5" style="width: 30rem;">
-            <div class="card-body transparent">
-                <div class="p-3">
-                    <div class="text-center">
-                        <img src="{{ asset('img/LOGO KKPM.jpg') }}" alt="Logo KKPM" width="50">
-                        <h1 class="h4 text-gray-900 mb-4"><b>Selamat Datang di Aplikasi KKPM</b></h1>
-                    </div>
-                    <hr>
-                    @if (session('error'))
-                        <div class="alert alert-danger">
-                            <b>Opps!</b> {{ session('error') }}
+    <div class="container" style="height: 100vh">
+        <div class="container2">
+            <div class="card border-primary shadow-lg my-5" style="width: 30rem;">
+                <div class="card-body transparent">
+                    <div class="p-3">
+                        <div class="text-center">
+                            <img src="{{ asset('img/LOGO KKPM.jpg') }}" alt="Logo KKPM" width="50">
+                            <h1 class="h4 text-gray-900 mb-4"><b>Selamat Datang di Aplikasi KKPM</b></h1>
                         </div>
-                    @endif
-                    <form action="{{ route('actionlogin') }}" method="post" class="user">
-                        @csrf
-                        <div class="form-group">
-                            <input type="email" name="email" class="form-control form-control-user"
-                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                placeholder="Enter Email Address..."required="">
-                        </div>
-                        <div class="form-group">
-                            <input type="password" name="password" class="form-control form-control-user"
-                                id="exampleInputPassword" placeholder="Password" required="">
-                        </div>
-                        <div class="form-group">
-                            <div class="custom-control custom-checkbox small">
-                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                <label class="custom-control-label" for="customCheck">Remember
-                                    Me</label>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn bg-gradient-primary btn-block">Login</button>
                         <hr>
-                    </form>
-                    <hr>
-                    <div class="text-center">
-                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                    </div>
-                    <div class="text-center">
-                        <a class="small" href="register.html">Create an Account!</a>
+                        @if (session('error'))
+                            <div class="alert alert-danger">
+                                <b>Opps!</b> {{ session('error') }}
+                            </div>
+                        @endif
+                        <form action="{{ route('actionlogin') }}" method="post" class="user">
+                            @csrf
+                            <div class="form-group">
+                                <input type="email" name="email" class="form-control form-control-user"
+                                    id="exampleInputEmail" aria-describedby="emailHelp"
+                                    placeholder="Enter Email Address..."required="">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="password" class="form-control form-control-user"
+                                    id="exampleInputPassword" placeholder="Password" required="">
+                            </div>
+                            <div class="form-group">
+                                <div class="custom-control custom-checkbox small">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck">
+                                    <label class="custom-control-label" for="customCheck">Remember
+                                        Me</label>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn bg-gradient-primary btn-block">Login</button>
+                            <hr>
+                        </form>
+                        <hr>
+                        <div class="text-center">
+                            <a class="small" href="forgot-password.html">Forgot Password?</a>
+                        </div>
+                        <div class="text-center">
+                            <a class="small" href="register.html">Create an Account!</a>
+                        </div>
                     </div>
                 </div>
             </div>
