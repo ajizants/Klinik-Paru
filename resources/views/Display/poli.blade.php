d834113ee4f4f59de70e26768125e524494b68c2
ajizants <13kalong13@gmail.com>
2025-04-24 11:07:02 +0700

    auah

--- resources/views/Display/poli.blade.php
+++ resources/views/Display/poli.blade.php
@@ -201,63 +201,7 @@
             </div>
         </div>
     </div>
-    <footer class="container-fluid fixed-bottom bg-primary">
-        <marquee class="marquee my-1" style="font-size: 2rem !important; color: #ffffff">
-            "Kamu seorang pejuang. Lawan penyakit yang ada di tubuhmu dan semoga segera sembuh."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Saya sangat menantikan kehadiranmu dengan penuh semangat. Segera sembuh, Sobat."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Kita memiliki banyak impian untuk dicapai bersama dan kita memiliki lebih banyak hal untuk
-            dicapai
-            dalam hidup. Cepat sembuh, Sayang." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Kamu pasti akan pulih karena saya tahu bahwa penyakitmu bisa dikalahkan dengan kekuatan dan
-            kemauanmu. Segera sembuh dan kembali lebih kuat." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Saya tahu kamu akan kembali lebih kuat dan lebih sehat, tidak ada yang bisa memenangkan tekad
-            dan
-            kekuatanmu. Semoga cepat sembuh." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Teman yang terkasih, percayalah semuanya akan baik-baik saja. Semoga cepat sembuh!"
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Jangan takut, Sahabatku, doamu didengar. Dia akan menaklukkanmu dan memberimu kemenangan. Cepat
-            Sembuh, Sob." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Semua pasti ada hikmahnya, jangan larut dalam kesedihan. Bersemangatlah karena itu akan membuat
-            keadaan lebih baik." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Semangat. Tak apa kamu sekarang terbujur lemas di ranjang rumah sakit ini. Aku yakin kamu bisa
-            melewati ini semua dan pulih seperti sedia kala." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Hal terpenting adalah jangan pernah putus asa. Aku selalu berdoa, semoga kamu cepat sembuh."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Anda sedang melalui situasi yang sulit, tetapi saya tahu Anda memiliki kekuatan untuk muncul
-            dengan
-            penuh kemenangan. Jaga diri Anda baik-baik dan jangan pernah menyerah!"
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Saya sangat mengagumi keberanian Anda menghadapi situasi ini. Anda adalah orang yang sangat
-            pejuang
-            dan saya tahu bahwa Anda akan menang. Saya mengirimi Anda pelukan hangat dan harapan terbaik
-            saya."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Ingatlah suatu hari, tidak lama lagi, kamu akan benar-benar sehat dan tersenyum kembali."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Kesembuhan memang butuh waktu dan kerja keras, tapi kamu tidak sendiri. Kami selalu
-            memikirkanmu
-            dan berdoa untuk kesembuhanmu." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Masa-masa sulit tidak bertahan lama, orang-orang tangguh melakukannya. Semoga cepat sembuh."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Percayalah bahwa setiap penyakit selalu ada obatnya. Kamu hanya perlu berpikir positif dan
-            bangkit
-            dari keputusasaan." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Janganlah kamu takut dengan rasa sakit, sebab dengan semangatmu, itu akan hilang. Aku akan
-            menemanimu dan merawatmu sampai kamu pulih dan sembuh. Lekas pulih ya."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Optimistislah, mulailah berpikir bahwa semuanya akan terjadi dan Anda akan segera mendapatkan
-            kembali kesehatan Anda." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Jadilah kuat karena segalanya akan menjadi lebih baik. Mungkin badai sekarang, tetapi tidak
-            pernah
-            hujan selamanya. Semoga cepat sembuh." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Anda tidak tampak hebat saat sakit. Jadi cepatlah sembuh agar Anda terlihat menarik kembali.
-            Semoga
-            Anda cepat pulih." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Rasa sakit itu nyata, tetapi begitu juga harapan. Semoga cepat sembuh."
-        </marquee>
-    </footer>
+    @include('Display.footer')
 
     <script type="text/javascript">
         let id = @json($id)
--- resources/views/Display/tensi2.blade.php
+++ resources/views/Display/tensi2.blade.php
@@ -130,7 +130,7 @@
                                         </span>
                                     </div>
                                     <div class="text-center py-4">
-                                        <span id="alamat_loket_{{ $index }}_lokasi" class="font-weight-bold"
+                                        <span id="alamat_loket_{{ $index }}" class="font-weight-bold"
                                             style="font-size: calc(3vw + 1rem); height: auto; line-height: 1;">
                                             {{ $item['kelurahan'] ?? '-' }}
                                         </span>
@@ -279,63 +279,7 @@
             </div>
         </div>
     </div>
-    <footer class="container-fluid fixed-bottom bg-primary">
-        <marquee class="marquee my-1" style="font-size: 2rem !important; color: #ffffff">
-            "Kamu seorang pejuang. Lawan penyakit yang ada di tubuhmu dan semoga segera sembuh."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Saya sangat menantikan kehadiranmu dengan penuh semangat. Segera sembuh, Sobat."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Kita memiliki banyak impian untuk dicapai bersama dan kita memiliki lebih banyak hal untuk
-            dicapai
-            dalam hidup. Cepat sembuh, Sayang." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Kamu pasti akan pulih karena saya tahu bahwa penyakitmu bisa dikalahkan dengan kekuatan dan
-            kemauanmu. Segera sembuh dan kembali lebih kuat." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Saya tahu kamu akan kembali lebih kuat dan lebih sehat, tidak ada yang bisa memenangkan tekad
-            dan
-            kekuatanmu. Semoga cepat sembuh." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Teman yang terkasih, percayalah semuanya akan baik-baik saja. Semoga cepat sembuh!"
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Jangan takut, Sahabatku, doamu didengar. Dia akan menaklukkanmu dan memberimu kemenangan. Cepat
-            Sembuh, Sob." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Semua pasti ada hikmahnya, jangan larut dalam kesedihan. Bersemangatlah karena itu akan membuat
-            keadaan lebih baik." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Semangat. Tak apa kamu sekarang terbujur lemas di ranjang rumah sakit ini. Aku yakin kamu bisa
-            melewati ini semua dan pulih seperti sedia kala." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Hal terpenting adalah jangan pernah putus asa. Aku selalu berdoa, semoga kamu cepat sembuh."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Anda sedang melalui situasi yang sulit, tetapi saya tahu Anda memiliki kekuatan untuk muncul
-            dengan
-            penuh kemenangan. Jaga diri Anda baik-baik dan jangan pernah menyerah!"
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Saya sangat mengagumi keberanian Anda menghadapi situasi ini. Anda adalah orang yang sangat
-            pejuang
-            dan saya tahu bahwa Anda akan menang. Saya mengirimi Anda pelukan hangat dan harapan terbaik
-            saya."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Ingatlah suatu hari, tidak lama lagi, kamu akan benar-benar sehat dan tersenyum kembali."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Kesembuhan memang butuh waktu dan kerja keras, tapi kamu tidak sendiri. Kami selalu
-            memikirkanmu
-            dan berdoa untuk kesembuhanmu." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Masa-masa sulit tidak bertahan lama, orang-orang tangguh melakukannya. Semoga cepat sembuh."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Percayalah bahwa setiap penyakit selalu ada obatnya. Kamu hanya perlu berpikir positif dan
-            bangkit
-            dari keputusasaan." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Janganlah kamu takut dengan rasa sakit, sebab dengan semangatmu, itu akan hilang. Aku akan
-            menemanimu dan merawatmu sampai kamu pulih dan sembuh. Lekas pulih ya."
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Optimistislah, mulailah berpikir bahwa semuanya akan terjadi dan Anda akan segera mendapatkan
-            kembali kesehatan Anda." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Jadilah kuat karena segalanya akan menjadi lebih baik. Mungkin badai sekarang, tetapi tidak
-            pernah
-            hujan selamanya. Semoga cepat sembuh." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Anda tidak tampak hebat saat sakit. Jadi cepatlah sembuh agar Anda terlihat menarik kembali.
-            Semoga
-            Anda cepat pulih." &nbsp;&nbsp;|&nbsp;&nbsp;
-            "Rasa sakit itu nyata, tetapi begitu juga harapan. Semoga cepat sembuh."
-        </marquee>
-    </footer>
+    @include('Display.footer')
     <script type="text/javascript">
         async function getList() {
             const norm = "";
@@ -384,15 +328,15 @@
 
                     const row = document.createElement("tr");
 
-                    const noUrut = document.createElement("td");
-                    noUrut.textContent = item.antrean_angka;
-                    row.appendChild(noUrut);
-                    noUrut.classList.add("col-2");
+                    const nama = document.createElement("td");
+                    nama.textContent = item.pasien_nama;
+                    row.appendChild(nama);
+                    nama.classList.add("col-2");
 
-                    const penjamin = document.createElement("td");
-                    penjamin.textContent = item.penjamin_nama;
-                    row.appendChild(penjamin);
-                    penjamin.classList.add("col-2");
+                    const loket = document.createElement("td");
+                    loket.textContent = item.ruang_nama;
+                    row.appendChild(loket);
+                    loket.classList.add("col-2");
 
                     const status = document.createElement("td");
                     status.textContent = item.keterangan;
@@ -475,14 +419,16 @@
             console.log("🚀 ~ data:", data);
 
             data.forEach((item, index) => {
+                console.log("🚀 ~ data.forEach ~ index:", index)
                 // Isi nama pasien
-                const namaEl = document.getElementById(`notif_loket_${index}`);
+                const namaEl = document.getElementById(`nama_loket_${index}`);
                 if (namaEl) {
+                    console.log("🚀 ~ data.forEach ~ namaEl:", namaEl)
                     namaEl.textContent = item.pasien_nama || '-';
                 }
 
                 // Isi kelurahan
-                const kelurahanEl = document.getElementById(`notif_loket_${index}_lokasi`);
+                const kelurahanEl = document.getElementById(`alamat_loket_${index}`);
                 if (kelurahanEl) {
                     kelurahanEl.textContent = item.kelurahan || '-';
                 }
