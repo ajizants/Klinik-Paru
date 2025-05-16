f14bb0702260b34b0b2f214f200d6f68eed229c6
Aksa_Kelana <46523947+ajizants@users.noreply.github.com>
2025-05-16 22:24:28 +0700

    tes

--- app/Http/Middleware/TrustProxiescopy.php
+++ app/Http/Middleware/TrustProxiescopy.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
@@ -19,16 +20,9 @@ class TrustProxies extends Middleware
      * @var int
      */
     protected $headers =
-    Request::HEADER_X_FORWARDED_FOR |
-    Request::HEADER_X_FORWARDED_HOST |
-    Request::HEADER_X_FORWARDED_PORT |
-    Request::HEADER_X_FORWARDED_PROTO |
-    Request::HEADER_X_FORWARDED_AWS_ELB;
-
-    //     protected $headers =
-    // Request::HEADER_X_FORWARDED_FOR |
-    // Request::HEADER_X_FORWARDED_HOST |
-    // Request::HEADER_X_FORWARDED_PORT |
-    // Request::HEADER_X_FORWARDED_PROTO;
-
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
