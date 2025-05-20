0d38d67cd795b238db6baed039d9ec61eb39efd6
ajizants <13kalong13@gmail.com>
2025-05-20 10:57:05 +0700

    indonesia raya

--- resources/views/IGD/Trans/main.blade.php
+++ resources/views/IGD/Trans/main.blade.php
@@ -7,30 +7,34 @@
     @include('IGD.Trans.antrian')
     @include('IGD.Trans.input')
 
-    <audio id="morning-audio" src="{{ asset('audio/Indonesia_Raya.mp3') }}" preload="auto"></audio>
+    <audio id="morning-audio" src="{{ asset('audio/IndonesiaRaya.mp3') }}" preload="auto"></audio>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             const audio = document.getElementById("morning-audio");
             audio.volume = 0.5; // Atur volume ke 50%
 
             function checkAndPlayAudio() {
+                // localStorage.setItem("audioPlayedDate", "");
+                console.log("🚀 ~ checkAndPlayAudio ~ checkAndPlayAudio:", checkAndPlayAudio)
                 const now = new Date();
                 const hours = now.getHours();
                 const minutes = now.getMinutes();
 
                 const playedToday = localStorage.getItem("audioPlayedDate");
+                console.log("🚀 ~ checkAndPlayAudio ~ playedToday:", playedToday)
                 const today = now.toISOString().split('T')[0]; // Format: yyyy-mm-dd
+                console.log("🚀 ~ checkAndPlayAudio ~ today:", today)
 
-                if (hours === 9 && minutes === 59 && playedToday !== today) {
+                if (hours === 10 && minutes === 00 && playedToday !== today) {
                     audio.play().then(() => {
                         localStorage.setItem("audioPlayedDate", today);
                     }).catch((err) => {
                         console.log("Audio tidak bisa diputar otomatis: ", err);
                     });
+                    console.log("🚀 ~ audio.play ~ localStorage:", localStorage)
 
                 }
             }
-
             setInterval(checkAndPlayAudio, 60000); // Cek setiap 1 menit
         });
     </script>
