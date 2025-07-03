c21f8e2ad1514a4a4fc346da0ffc65d135374f11
ajiZant <13Kalong13@gmail.com>
2025-07-03 11:50:42 +0000

    tambahan cuti

---
+++ app/Models/CutiTambahan.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class cutiTambahan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'peg_t_cuti_tambahan';
+
+    protected $fillable = [
+        'id',
+        'nip',
+        'jumlah_tambahan',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function pegawai()
+    {
+        return $this->belongsTo(Vpegawai::class, 'nip', 'nip');
+    }
+}
